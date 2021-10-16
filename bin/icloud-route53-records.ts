8acc2984020021b9029d0869df12a2483055ece9
Matthew Murray <matthew@compti.me>
2021-10-16 10:09:14 +0100

    Set DNS records for iCloud+ custom domain email

--- bin/icloud-route53-records.ts
+++ bin/icloud-route53-records.ts
@@ -11,7 +11,9 @@ new IcloudRoute53RecordsStack(app, 'IcloudRoute53RecordsStack', {
 
   /* Uncomment the next line to specialize this stack for the AWS Account
    * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+   domainName: "compti.me",
+   txtRecord: "apple-domain=wSYPFyOW5yQsyk25"
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
--- lib/icloud-route53-records-stack.ts
+++ lib/icloud-route53-records-stack.ts
@@ -1,9 +1,43 @@
 import * as cdk from '@aws-cdk/core';
+import * as route53 from '@aws-cdk/aws-route53';
+  
+export interface IcloudRoute53RecordsProps extends cdk.StackProps {
+  domainName: string;
+  txtRecord: string;
+}
 
 export class IcloudRoute53RecordsStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.Construct, id: string, props: IcloudRoute53RecordsProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const zone = route53.HostedZone.fromLookup(this, "Zone", { domainName: props.domainName });
+
+    new route53.MxRecord(this, "MxRecords", {
+      values: [
+        {
+          hostName: "mx01.mail.icloud.com.",
+          priority: 10
+        },
+        {
+          hostName: "mx02.mail.icloud.com.",
+          priority: 10
+        }
+      ],
+      zone,
+    });
+
+    new route53.TxtRecord(this, "TxtRecord", {
+      values: [
+        props.txtRecord,
+        "v=spf1 redirect=icloud.com"
+      ],
+      zone
+    })
+
+    new route53.CnameRecord(this, "CnameRecord", {
+      recordName: "sig1._domainkey",
+      domainName: "sig1.dkim.compti.me.at.icloudmailadmin.com.",
+      zone
+    })
   }
 }
