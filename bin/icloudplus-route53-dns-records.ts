cf4a625dd94a52aaef0a2fda50e07db87e18ef1a
Matthew Murray <matthew@compti.me>
2021-10-16 11:01:37 +0100

    Actually use the DOMAIN_NAME env var

--- bin/icloudplus-route53-dns-records.ts
+++ bin/icloudplus-route53-dns-records.ts
@@ -11,9 +11,9 @@ new IcloudplusRoute53DnsRecordsStack(app, 'IcloudplusRoute53DnsRecordsStack', {
 
   /* Uncomment the next line to specialize this stack for the AWS Account
    * and Region that are implied by the current CLI configuration. */
-   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-   domainName: "compti.me",
-   txtRecord: process.env.TXT_RECORD!
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+  domainName: process.env.DOMAIN_NAME!,
+  txtRecord: process.env.TXT_RECORD!
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
   // env: { account: '123456789012', region: 'us-east-1' },
